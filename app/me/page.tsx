844fcb34b5b6c7d98b8ca82bd360c21507b0884e
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-09 00:29:38 +0700

    create page show profile

---
+++ app/me/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@nextui-org/button";
+const ProfilePage = () => {
+  return (
+    <div>
+      <Button color="primary">Button</Button>
+    </div>
+  );
+};
+
+export default ProfilePage;
