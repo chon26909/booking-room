62dbb641a6ec618cbd0f5ecb6526676c5fdb0b86
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-12 20:30:21 +0700

    profile page

--- app/me/Profile.tsx
+++ app/me/Profile.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { FC } from 'react';
 
-const Profile = () => {
-    return <div>Profile</div>;
+export interface IUser {
+    firstname: string;
+    lastname: string;
+    email: string;
+}
+interface IProfileProps {
+    data: IUser;
+}
+
+const Profile: FC<IProfileProps> = (props) => {
+    const {
+        data: { firstname, lastname, email }
+    } = props;
+
+    return (
+        <div className='w-[500px] mx-auto'>
+            <div className='mt-10 bg-red-300 p-5 rounded-md text-xl'>
+                <div className='text-center font-bold text-2xl mb-5'>My Profile</div>
+                <div>firstname: {firstname}</div>
+                <div>lastname: {lastname}</div>
+                <div>email: {email}</div>
+            </div>
+        </div>
+    );
 };
 
 export default Profile;
