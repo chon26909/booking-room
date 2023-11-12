7c63569a7d9c486bbe75392140a4af1f5957077e
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-13 00:56:01 +0700

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,16 @@
 import Toast from '@/components/Toast';
 import { useToastStore } from '@/store/toast';
 import { Button } from '@nextui-org/react';
-import { useEffect } from 'react';
 
 export default function Home() {
     const { toast } = useToastStore();
 
     return (
         <main>
-            <Button className='mt-52' onClick={() => toast.success('Update Successfully', { duration: 10000 })}>
+            <Button
+                className='mt-52'
+                onClick={() => toast.success('Update Successfully', { duration: 10000 })}
+            >
                 show toast
             </Button>
             <Toast bodyClassName='p-5 w-[500px]' />
