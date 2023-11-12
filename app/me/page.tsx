9847eba47ab56111f838e34beded836b8e444528
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-12 20:30:08 +0700

    update page

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import { Button } from '@nextui-org/button';
+'use client';
+
+import React, { createContext, useEffect, useState } from 'react';
 import FormLogin from './FormLogin';
+import Profile, { IUser } from './Profile';
+import axios from 'axios';
+// const UserContext = createContext(initialState);
+
 const ProfilePage = () => {
+    const [accessToken, setAccessToken] = useState('');
+
+    const [userData, setUserData] = useState<IUser>();
+
+    useEffect(() => {
+        const getUserProfile = async () => {
+            const { data } = await axios.get('http://localhost:4000/auth/me', {
+                headers: {
+                    authorization: 'Bearer ' + accessToken
+                }
+            });
+            console.log('user profile', data);
+            setUserData(data.data);
+        };
+
+        if (accessToken) {
+            getUserProfile();
+        }
+    }, [accessToken]);
+
     return (
-        <div>
-            <FormLogin />
-        </div>
+        <>
+            {userData ? <Profile data={userData} /> : <FormLogin setAccessToken={setAccessToken} />}
+        </>
     );
 };
 
