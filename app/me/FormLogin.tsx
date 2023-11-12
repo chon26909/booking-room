8cf450c9ce44326203e94fd058c0848791b644f3
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-12 20:29:45 +0700

    login form

--- app/me/FormLogin.tsx
+++ app/me/FormLogin.tsx
@@ -6,30 +6,35 @@ import React, { useRef } from 'react';
 import { useMutation } from '@tanstack/react-query';
 
 interface ILoginRequest {
-    username: string;
+    email: string;
     password: string;
 }
 
-const FormLogin = () => {
+interface IFormLoginProps {
+    setAccessToken(token: string): void;
+}
+
+const FormLogin = (props: IFormLoginProps) => {
     const { mutate } = useMutation({
         mutationFn: (body: ILoginRequest) => login(body),
-        onSuccess(data) {
-            console.log('data', data);
+        onSuccess({ data }) {
+            console.log('token', data.data);
+            props.setAccessToken(data.data.accessToken);
         }
     });
 
-    const usernameRef = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
 
     const login = (body: ILoginRequest) => {
-        return axios.post('https://dummyjson.com/auth/login', body);
+        return axios.post('http://localhost:4000/auth/login', body);
     };
 
     const handleSubmit = () => {
         console.log('handleSubmit');
 
         const data = {
-            username: usernameRef.current?.value || '',
+            email: emailRef.current?.value || '',
             password: passwordRef.current?.value || ''
         };
 
@@ -38,10 +43,10 @@ const FormLogin = () => {
 
     return (
         <div className='bg-blue-300 min-h-screen flex justify-center items-center'>
-            <div className='bg-white p-7 w-[500px]  rounded-md'>
+            <div className='bg-white p-7 w-[500px] rounded-md'>
                 <div className='text-3xl font-bold text-center my-5'>Login </div>
-                <Input ref={usernameRef} type='text' label='Username' variant='bordered' className='w-full my-5' defaultValue='kminchelle' />
-                <Input ref={passwordRef} type='password' label='Password' variant='bordered' className='w-full my-5' defaultValue='0lelplR' />
+                <Input ref={emailRef} type='text' label='Email' variant='bordered' className='w-full my-5' defaultValue='demo@gmail.com' />
+                <Input ref={passwordRef} type='password' label='Password' variant='bordered' className='w-full my-5' defaultValue='1234' />
                 <Button fullWidth onClick={handleSubmit}>
                     Submit
                 </Button>
